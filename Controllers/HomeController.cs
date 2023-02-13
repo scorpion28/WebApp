5cca2841400d4218d9ded3db1b430f59fcfe9a4b
scorpion28 <z92k1t@gmail.com>
2023-02-13 12:25:44 +0200

    feat: HomeController

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApp.Models;
+
+namespace WebApp.Controllers;
+
+public class HomeController : Controller
+{
+    private DataContext context;
+
+    public HomeController(DataContext ctx)
+    {
+        context = ctx;
+    }
+
+    public async Task<IActionResult> Index(long id = 1)
+    {
+        Product? prod = await context.Products.FindAsync(id);
+        if (prod?.CategoryId == 1) {
+            return View("Watersport", prod);
+        } else {
+            return View(prod);
+        }
+    }
+    
+    public IActionResult Common() {
+        return View();
+    }
+}
